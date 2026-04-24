f3fbfae1940227cf3c6d291a966d2bf6220844e3
decarogregory-ux <decaro.gregory@gmail.com>
2026-04-24 17:50:02 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,24 @@
-import './styles/globals.css'
+import Head from 'next/head'
+import '../styles/globals.css'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>LiveOnChain</title>
+
+        {/* PWA SETTINGS */}
+        <meta name="theme-color" content="#00f5a0" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="LiveOnChain" />
+
+        {/* ICON + MANIFEST */}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
