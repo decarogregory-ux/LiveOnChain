2363562ff245b5b69a628de5e4e5ce277a7aed80
decarogregory-ux <decaro.gregory@gmail.com>
2026-05-01 19:17:00 -0400

    Remove comments from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,17 @@
 import Head from 'next/head'
-import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>LiveOnChain</title>
-
-        {/* PWA SETTINGS */}
         <meta name="theme-color" content="#00f5a0" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="LiveOnChain" />
-
-        {/* ICON + MANIFEST */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
       </Head>
-
       <Component {...pageProps} />
     </>
   )
